ea8bcdb043d41172d2dbe798f1115a06df5586fd
kanomatahiroyuki <hiroyuki.kanomata.rks@gmail.com>
2021-11-18 10:31:03 +0900

    first commit

---
+++ src/main/java/com/example/Car.java
@@ -0,0 +1,48 @@
+package com.example;
+
+/**
+ * 車のドメイン
+ * 
+ * @author kano
+ *
+ */
+public class Car {
+
+	private int speed;
+	private String name;
+	
+	/**
+	 * 引数なしコンストラクタ
+	 */
+	public Car() {}
+	
+	/**
+	 * 初期化用の引数ありコンストラクタ
+	 * 
+	 * @param speed 速度
+	 * @param name 名前
+	 */
+	public Car(int speed, String name) {
+		super();
+		this.speed = speed;
+		this.name = name;
+	}
+	
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [speed=" + speed + ", name=" + name + "]";
+	}	
+}
